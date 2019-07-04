d7938112f000a186ecfabf4fcd11ca911dbdf661
Jacob Stewart <jacobstewart1987@gmail.com>
2019-07-04 12:46:39 -0700

    finished

---
+++ JAVASCRIPT_LEVEL_ONE/part3Exercise.js
@@ -0,0 +1,6 @@
+var imperialWeight = Number(prompt("What is the weight in pounds?"));
+var metricWeight = imperialWeight * 0.454;
+alert("In the units that didn't make it to the moon,\n"
+	+ "that is " + metricWeight + "kg.");
+console.log("Conversion Complete")
+
---
+++ JAVASCRIPT_LEVEL_ONE/part9.js
@@ -0,0 +1,11 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is you age?");
+var height = prompt("What is your height in centimeters?");
+var petName = prompt("What is the name of your pet?");
+if (firstName[0].toUpperCase() === lastName[0].toUpperCase()
+	&& (age < 30 && age > 20)
+	&& (height >= 170)
+	&& petName[petName.length - 1].toLowerCase() ==="y"){
+	console.log("UUDDLRLRBAS");
+};
