49a0cacdc5593195deaa44e80d9d2d6a196acfd6
Jacob Stewart <jacobstewart1987@gmail.com>
2019-07-09 00:13:01 -0700

    fixed mappings

--- Django/ProTwo/AppTwo/urls.py
+++ Django/ProTwo/AppTwo/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import url 
+from django.urls import path 
 from AppTwo import views
 
 urlpatterns = [
-        url(r'^$', views.index, name='help'),
+  #      url(r'^$', views.index, name='help'),
+        path('', views.index, name='index'),
+        path('users/', views.users, name='users'),
         ]
 
