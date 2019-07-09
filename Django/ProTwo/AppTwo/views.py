503db981e6f41211646191603bf05d2c8095de99
Jacob Stewart <jacobstewart1987@gmail.com>
2019-07-09 00:15:18 -0700

    added view for users/

--- Django/ProTwo/AppTwo/views.py
+++ Django/ProTwo/AppTwo/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from AppTwo.models import User
 
 # Create your views here.
 
 
 def index(request):
-    my_dict = {'insert_here':'This is a help page'}
+    my_dict = {'insert_here':'Go to /users to see the list of user info'}
     return render(request, 'AppTwo/help.html', context=my_dict)
+
+
+def users(request):
+    usr_list = User.objects.order_by('last_name')
+    usr_dict = {'all_users':usr_list}
+    return render(request, 'AppTwo/usr.html', context=usr_dict)
