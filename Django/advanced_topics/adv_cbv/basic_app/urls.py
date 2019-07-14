a9dcfa0d88e4f28ad227b8f6652252198d866e9b
Jacob Stewart <jacobstewart1987@gmail.com>
2019-07-13 17:06:06 -0700

    Finished CRUD

--- Django/advanced_topics/adv_cbv/basic_app/urls.py
+++ Django/advanced_topics/adv_cbv/basic_app/urls.py
@@ -13,12 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include,re_path
+from django.urls import path, re_path
 from basic_app import views
 
+app_name='basic_app'
+
+
 urlpatterns = [
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^$', views.IndexView.as_view()),
-    re_path(r'^basic_app/', include('basic_app.urls', namespace='basic_app')),
+    re_path(r'^$', views.SchoolListView.as_view(), name='list'),
+    re_path(r'^(?P<pk>\d+)/$',
+            views.SchoolDetailView.as_view(),
+            name='detail'),
+    path('create/', views.SchoolCreateView.as_view(), name='create'),
+    re_path(r'^update/(?P<pk>\d+)/$',
+            views.SchoolUpdateView.as_view(),
+            name='update'),
+    re_path(r'^delete/(?P<pk>\d+)/$',
+            views.SchoolDeleteView.as_view(),
+            name='delete'),
 ]
