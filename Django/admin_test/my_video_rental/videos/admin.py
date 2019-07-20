d7cc7c90351dd5f9367371f7a7c0d0f4e8d49914
Jacob Stewart <jacobstewart1987@gmail.com>
2019-07-20 12:10:26 -0700

    messing with admin views

---
+++ Django/admin_test/my_video_rental/videos/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['release_year','title','length']
+    search_fields = ['title', 'length']
+    list_filter=['release_year','length','title']
+    list_display = ['title','length','release_year']
+    list_editable = ['length']
+
+
+admin.site.register(models.Customer)
+admin.site.register(models.Movie,MovieAdmin)
